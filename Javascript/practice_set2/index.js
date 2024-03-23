b49630d2b16c19101d89e50b8f660af0bb1f4339
MridulShrimal <120498556+MridulShrimal@users.noreply.github.com>
2024-03-23 18:58:11 +0530

    Add files via upload

---
+++ Javascript/practice_set2/index.js
@@ -0,0 +1,4 @@
+let quantity = 0;
+document.querySelector('#cartSummary').innerText = `Your bag has ${quantity} items`;
+
+
---
+++ Javascript/practice_set3/index.js
@@ -0,0 +1,5 @@
+const myName = 'Mridul';
+document.querySelector('#my_para').innerText = myName;
+//in eval input is given in form of string/
+console.log(eval('3+5'))
+console.log(5+7)
